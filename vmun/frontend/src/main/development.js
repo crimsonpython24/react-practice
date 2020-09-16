77cea3fbd02529cd131ca0575cd0f4ac643920f2
Warren Wang <warrenyjwang@gmail.com>
2020-09-16 20:32:11 +0800

    #0008 -- added todo routing but nothing else, bug present, read file

--- vmun/frontend/src/main/development.js
+++ vmun/frontend/src/main/development.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, useRouteMatch, Link } from "react-router-dom";
 import "antd/dist/antd.css";
+import Todo from "../todo/index";
 
+// TODO (ha ha): fix styles and routing format
+// Routing present in index.js (outmost)
 function Development() {
-  return <h1>Development</h1>
+  let { path, url } = useRouteMatch();
+  return (
+    <>
+      <Router>
+        {/* <Link to={`${url}/todo`}>Todos</Link> */}
+        {/* <Switch>
+          <Route exact path={path}></Route>
+          <Route exact path={`${path}/todo`}>
+            <Todo/>
+          </Route>
+        </Switch> */}
+      </Router>
+    </>
+  )
 }
 
 export default Development;
---
+++ vmun/frontend/src/todo/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "antd/dist/antd.css";
+
+function Todo() {
+  return (
+    <h1>update</h1>
+  )
+}
+
+export default Todo;
